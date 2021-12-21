28d4dcc5f0e6cbe385b33520a4236310b4326579
mosesmbugua <swahili1963@gmail.com>
2021-12-20 22:10:20 -0800

    Adjust the Cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -16,6 +16,9 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Box } from "@mui/system";
 import { Rating } from "@mui/material";
+
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -35,23 +38,14 @@ export default function Cards() {
   };
 
   const reviews = 9;
+  const totalRestaurants = 10000;
+  const currentRanking = 5000;
+  const city = "New York City";
 
+  const address = "New York City 9800032-5224";
+  const phoneNumber = "425-254-4392";
   return (
     <Card sx={{ maxWidth: 500 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
       <CardMedia
         component="img"
         height="220"
@@ -62,19 +56,48 @@ export default function Cards() {
         <Typography variant="h5" color="text.secondary">
           375 Chicken 'n Fries
         </Typography>
-        <Box sx={{ width: 200, display: "flex" }}>
+        <Box sx={{ width: "100%", display: "flex" }}>
           <Rating
             name="half-rating-read"
             defaultValue={2.5}
             precision={0.5}
             readOnly
           />
-          <Box sx={{ flexGrow: 3 }} />
+          <Box sx={{ flexGrow: 1 }} />
 
-          <Typography variant="body2" color="text.secondary">
+          <Typography align="right" variant="body2" color="text.secondary">
             {reviews} reviews
           </Typography>
         </Box>
+        {/* <Box sx={{ width: 200, display: "flex" }}>
+          <Typography align="left" variant="body2" color="text.secondary">
+            Ranking
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+
+          <Typography align="left" variant="body2" color="text.secondary">
+            #{currentRanking} of {totalRestaurants} in {city}
+          </Typography>
+        </Box>
+        <Box sx={{ flexGrow: 1 }} />
+
+        <Box sx={{ width: 300, display: "flex" }}>
+          <LocationOnIcon color="disabled" />
+          <Box sx={{ flexGrow: 1 }} />
+          <Typography align="left" variant="body2" color="text.secondary">
+            {address}
+          </Typography>
+        </Box>
+
+        <Box sx={{ width: 200, display: "flex" }}>
+          <LocalPhoneIcon color="disabled" />
+          <Box sx={{ flexGrow: 1 }} />
+          <Typography align="left" variant="body2" color="text.secondary">
+            {phoneNumber}
+          </Typography>
+        </Box>
+       */}
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
