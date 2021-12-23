4e421361ad6bca5623b8f85d229f2f7384d53c04
mosesmbugua <swahili1963@gmail.com>
2021-12-23 15:25:06 -0800

    Add Google Maps Base

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,34 +1,45 @@
-import * as React from "react";
+import React, { useEffect, useRef, ReactElement } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import GoogleMapReact from "google-map-react";
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import googleMapReact from "google-map-react";
+import { Loader } from "@googlemaps/js-api-loader";
 
 require("dotenv").config();
-class SimpleMap extends React.Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33,
-    },
-    zoom: 11,
-  };
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.GoogleMapApiKey }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-}
+const render = (status) => {
+  if (status === Status.LOADING) return <h3>{status}...</h3>;
+  if (status === Status.FAILURE) return <h3>{status}...</h3>;
+  return null;
+};
+
+const MyMapComponent = ({ center, zoom, style }) => {
+  const ref = useRef();
+  const [map, setMap] = React.useState();
+
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, { center, zoom }));
+    }
+  }, [ref, map, center, zoom]);
+
+  return <div ref={ref} id="map" style={style} />;
+};
+const SimpleMap = () => {
+  const center = { lat: 48.864716, lng: 2.349014 };
+  const zoom = 11;
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <Wrapper apiKey={process.env.GoogleMapApiKey} render={render}>
+        <MyMapComponent
+          center={center}
+          zoom={zoom}
+          style={{ flexGrow: "1", height: "100%" }}
+        />
+      </Wrapper>
+    </div>
+  );
+};
 
 const GoogleMap = () => {
   return (
