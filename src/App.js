1ebecaa208466e2dc38a5867f0c89239d4eecef8
mosesmbugua <swahili1963@gmail.com>
2021-12-25 20:16:41 -0800

    Add Hotel and Restaurants Routes API

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import PrimarySearchAppBar from "./components/NavBar";
 import MainContent from "./components/MainContent";
 import PlacesList from "./components/PlacesList";
 import { CssBaseline } from "@mui/material";
-import { getLocations } from "./api/TravelAdvisorAPI";
+import {
+  getLocations,
+  getHotels,
+  getRestaurants,
+} from "./api/TravelAdvisorAPI";
 import { useState, useEffect } from "react";
 function App() {
   const [searchQuery, setSearchQuery] = useState("new york city");
 
+  const [map, setMap] = useState({});
+  const [places, setPlaces] = useState([]);
+  const [type, setType] = useState("hotels");
+  //since the hotel and restaurants route require a longitude and latitude
+  //we will first call the locations route then get the longitude and latitude from it
+  const coordinates = {
+    latitude: 12.248278039408776,
+    longitude: 109.1981618106365,
+  };
   useEffect(() => {
     getLocations(searchQuery).then((response) => {
       console.log(response);
       if (response.status < 399) {
         console.log(response.data);
+        //set the coordinates from the result
       } else {
         console.log(response.status);
         console.log("check the query data");
       }
+
+      // if (type === "hotels") {
+      //   getHotels(coordinates).then((response) => {
+      //     //set the places data for hotels
+      //   });
+      // } else {
+      //   getHotels(coordinates).then((response) => {
+      //     //set the places data for restaurants
+      //   });
+      // }
     });
   });
 
@@ -25,7 +49,7 @@ function App() {
     <>
       <CssBaseline />
       <PrimarySearchAppBar setSearchQuery={setSearchQuery} />
-      <MainContent />
+      <MainContent map={map} places={places} setType={setType} />
     </>
   );
 }
--- src/api/TravelAdvisorAPI.js
+++ src/api/TravelAdvisorAPI.js
@@ -12,10 +12,83 @@ export const getLocations = async (query) => {
       },
     };
 
-    // const {
-    //   data: { data },
-    // } = await axios.request(options);
-    // return data;
+    const response = await axios.request(options);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getHotels = async (coordinates) => {
+  try {
+    var options = {
+      method: "POST",
+      url: "https://travel-advisor.p.rapidapi.com/hotels/v2/list",
+      params: { currency: "USD", units: "km", lang: "en_US" },
+      headers: {
+        "content-type": "application/json",
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_RAPID_API_TRAVE_ADVISOR_API_KEY,
+      },
+      data: {
+        geoId: 293928,
+        checkIn: "2021-06-28",
+        checkOut: "2021-06-30",
+        sort: "PRICE_LOW_TO_HIGH",
+        sortOrder: "asc",
+        filters: [
+          { id: "deals", value: ["1", "2", "3"] },
+          { id: "price", value: ["31", "122"] },
+          { id: "type", value: ["9189", "9201"] },
+          { id: "amenity", value: ["9156", "9658", "21778", "9176"] },
+          { id: "distFrom", value: ["2227712", "25.0"] },
+          { id: "rating", value: ["40"] },
+          { id: "class", value: ["9572"] },
+        ],
+        rooms: [
+          { adults: 2, childrenAges: [2] },
+          { adults: 2, childrenAges: [3] },
+        ],
+        boundingBox: {
+          northEastCorner: coordinates,
+          southWestCorner: coordinates,
+        },
+        updateToken: "",
+      },
+    };
+
+    const response = axios.request(options);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRestaurants = async (coordinates) => {
+  try {
+    var options = {
+      method: "POST",
+      url: "https://travel-advisor.p.rapidapi.com/restaurants/v2/list",
+      params: { currency: "USD", units: "km", lang: "en_US" },
+      headers: {
+        "content-type": "application/json",
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_RAPID_API_TRAVE_ADVISOR_API_KEY,
+      },
+      data: {
+        geoId: 293928,
+        partySize: 2,
+        reservationTime: "2021-07-07T20:00",
+        sort: "POPULARITY",
+        sortOrder: "desc",
+        filters: [{ id: "establishment", value: ["10591"] }],
+        boundingBox: {
+          northEastCorner: coordinates,
+          southWestCorner: coordinates,
+        },
+        updateToken: "",
+      },
+    };
 
     const response = await axios.request(options);
     return response;
